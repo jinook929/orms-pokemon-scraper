9efa993a5851a17dc24237da86d1706872e34670
jinook929 <jinook929@gmail.com>
2021-02-11 10:30:25 -0500

    commit tree 2f0af7e1a15d7d71a3e7194f1a4ce7410a0963f4

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:nil, name:, type:, db:)
+    # @id = id
+    # @name = name
+    # @type = type
+    # @db = db
+    @id, @name, @type, @db = id, name, type, db
+  end
+  
+  def self.save(name, type, db)
+    new_mon = self.new(name: name, type: type, db: db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES
+      (?, ?);
+    SQL
+    db.execute(sql, new_mon.name, new_mon.type)
+
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon;")[0][0]
+    new_mon
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = ?;
+    SQL
+    # found = db.execute(sql, id)
+    # self.new(id: found[0][0], name: found[0][1], type: found[0][2], db:@db)
+    found = db.execute(sql, id).flatten
+    self.new(id: found[0], name: found[1], type: found[2], db:db)
+  end
 end
